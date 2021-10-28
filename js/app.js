67f2e7eb2846dadce686a5b5e13dda35720a34a4
camiloans <camiloanso90@gmail.com>
2021-10-27 21:01:31 -0300

    Primer version Portfolio

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//Evento Submit
+$( "#formulario" ).submit(function( event ) {
+    alert( "Formulario Enviado");
+    event.preventDefault();
+  });
+
+//InsertAfter
+$( "<p>Acepto términos y condiciones.</p>" ).insertAfter( ".termYcond" );
+
+//Click
+$( "#calculadoraBtn" ).click(function() {
+    alert( "Disponible a partir del 7/11/21" );
+  });
+
+
+const inputNombre = formulario.nombreUsuario;
+const inputEmail = formulario.correoUsuario;
+
+
+inputNombre.addEventListener("focus", () => 
+    console.log("El usuario esta en el campo Nombre")
+);
+
+inputEmail.addEventListener("focus", () => 
+    console.log("El usuario esta en el campo Email")
+);
+
+const inputCheckbox = formulario.terminosCondiciones;
+inputCheckbox.addEventListener("change", (e) => {
+    if(e.target.checked == true){
+        console.log("El usuario acepta terminos y condiciones");
+    }else{
+        console.log("El usuario NO acepta los terminos y condiciones");
+    }
+});
